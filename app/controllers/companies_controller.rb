9e054a26598e4e88f9c1adde8f028fb077b603a9
jamessoftech <james.abraham.softech@gmail.com>
2017-03-25 14:00:41 -0700

    Implemented back-end api for rails

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,22 @@ class CompaniesController < ApplicationController
   def index
     render json: Company.all
   end
+
+  def create_company
+  	company = Company.new
+  	company.name = company_params[:name]
+  	render json: company.save ? "Success" : "Failed"
+  end
+
+  def create_fakers
+  	(0..100).each do |i|
+  		Company.create(name: Faker::Company.unique.name)
+  	end
+  end
+
+  private
+  
+  def company_params
+  	params.permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :companies, only: [:index]
+  get 'companies/create_company'
+  get 'companies/create_fakers'
 end
+
